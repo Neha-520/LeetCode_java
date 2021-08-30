68cf13d5243cdc3dad3203adf85bdc2c10de7209
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-08-30 18:09:23 +0530

    pascalTriangle

---
+++ DynamicProgramming/_118pascalsTriangle.java
@@ -0,0 +1,28 @@
+package DynamicProgramming;
+
+import java.util.*;
+
+public class _118pascalsTriangle {
+    class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> result = new ArrayList();
+            if (numRows == 0)
+                return result;
+            List<Integer> temp = new ArrayList();
+            temp.add(1);
+            result.add(temp);
+            for (int i = 1; i < numRows; i++) {
+                List<Integer> prev = result.get(i - 1);
+                List<Integer> curr = new ArrayList();
+                curr.add(1);
+                for (int j = 1; j < i; j++) {
+                    curr.add(prev.get(j - 1) + prev.get(j));
+                }
+                curr.add(1);
+                result.add(curr);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ DynamicProgramming/_119PascalTriangle2.java
@@ -0,0 +1,30 @@
+package DynamicProgramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _119PascalTriangle2 {
+    class Solution {
+        public List<Integer> getRow(int rowIndex) {
+            List<Integer> ans = new ArrayList<>();
+            ans.add(1);
+            if (rowIndex == 0)
+                return ans; // [1]
+            ans.add(1);
+            if (rowIndex == 1)
+                return ans; // [1, 1]
+            int count = 1; // rowIndex > 1
+            while (count <= rowIndex) {
+                List<Integer> row = new ArrayList<>();
+                row.add(1);
+                for (int i = 0; i < count - 1; i++) {
+                    row.add(ans.get(i) + ans.get(i + 1)); // calculating new row from previous/ans row
+                }
+                row.add(1);
+                ans = row; // setting previous row as new_row
+                count++;
+            }
+            return ans;
+        }
+    }
+}
