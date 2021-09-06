0c42388f32bc5b974b9421d29010a98d7ca19855
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-09-07 02:15:45 +0530

    116 617 700 1629

---
+++ Arrays/_1629SlowestKey.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class _1629SlowestKey {
+    class Solution {
+        public char slowestKey(int[] releaseTimes, String keysPressed) {
+            int[] b = new int[releaseTimes.length];
+            b[0] = releaseTimes[0];
+            for (int i = 1; i < releaseTimes.length; i++) {
+                b[i] = releaseTimes[i] - releaseTimes[i - 1];
+            }
+            int max = 0;
+            for (int i = 0; i < b.length; i++) {
+                max = Math.max(max, b[i]);
+            }
+            ArrayList<Character> a = new ArrayList<>();
+            for (int i = 0; i < b.length; i++) {
+                if (max == b[i])
+                    a.add(keysPressed.charAt(i));
+            }
+            if (a.size() == 1) {
+                return a.get(0);
+            }
+            char[] m = new char[a.size()];
+            for (int i = 0; i < m.length; i++) {
+                m[i] = a.get(i);
+            }
+            Arrays.sort(m);
+            return m[m.length - 1];
+        }
+    }
+}
---
+++ BinarySearchTree/_700SearchinBinarySearchTree.java
@@ -0,0 +1,37 @@
+package BinarySearchTree;
+
+public class _700SearchinBinarySearchTree {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            if (root == null)
+                return root;
+            else if (root.val > val)
+                return searchBST(root.left, val);
+            else if (root.val < val)
+                return searchBST(root.right, val);
+            else
+                return root;
+        }
+    }
+}
