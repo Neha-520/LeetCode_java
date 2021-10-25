719866e81cd34cc6cce703f1e7f574e0f6950f09
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-10-26 00:44:42 +0530

    vertical order traversal in tree + TreeMap

---
+++ Practice/VerticalOrderTraversal.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+}
+
+class BinaryTree {
+
+    public void printVerticalOrder(Node node) {
+        if (node == null)
+            return;
+        int hd = 0;
+        TreeMap<Integer, ArrayList<Integer>> m = new TreeMap<Integer, ArrayList<Integer>>();
+
+        getVerticalOrder(node, hd, m);
+
+        for (Map.Entry<Integer, ArrayList<Integer>> cm : m.entrySet()) {
+            System.out.println(cm.getValue());
+        }
+    }
+
+    private void getVerticalOrder(Node node, int hd, TreeMap<Integer, ArrayList<Integer>> m) {
+        if (node == null)
+            return;
+        if (m.get(hd) == null) {
+            ArrayList<Integer> l = new ArrayList<Integer>();
+            l.add(node.data);
+            m.put(hd, l);
+        } else {
+            ArrayList<Integer> l = m.get(hd);
+            l.add(node.data);
+            m.put(hd, l);
+        }
+
+        getVerticalOrder(node.left, hd - 1, m);
+        getVerticalOrder(node.right, hd + 1, m);
+    }
+
+    public Node createNewNode(int val) {
+        Node newNode = new Node();
+        newNode.data = val;
+        newNode.left = null;
+        newNode.right = null;
+        return newNode;
+    }
+}
+
+public class VerticalOrderTraversal {
+    public static void main(String[] args) {
+        BinaryTree a = new BinaryTree();
+
+        Node root = a.createNewNode(2);
+        root.left = a.createNewNode(7);
+        root.right = a.createNewNode(5);
+        root.left.left = a.createNewNode(2);
+        root.left.right = a.createNewNode(6);
+        root.left.right.left = a.createNewNode(5);
+        root.left.right.right = a.createNewNode(11);
+        root.right.right = a.createNewNode(9);
+        root.right.right.left = a.createNewNode(4);
+
+        a.printVerticalOrder(root);
+    }
+}
