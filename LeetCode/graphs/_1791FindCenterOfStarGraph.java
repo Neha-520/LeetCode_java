74ed7a251a550da5b40d96ba0a08cf30c5e39720
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-12-22 00:35:31 +0530

    1791

---
+++ LeetCode/graphs/_1791FindCenterOfStarGraph.java
@@ -0,0 +1,11 @@
+public class _1791FindCenterOfStarGraph {
+    class Solution {
+        public int findCenter(int[][] edges) {
+            int x = edges[0][0];
+            int y = edges[0][1];
+            if (edges[1][0] == x || edges[1][1] == x)
+                return x;
+            return y;
+        }
+    }
+}
