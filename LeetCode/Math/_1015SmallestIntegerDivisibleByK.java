e00ae4c735a51a573411f4c9e7e08d5117bd3340
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-12-31 00:01:55 +0530

    1015

---
+++ LeetCode/Math/_1015SmallestIntegerDivisibleByK.java
@@ -0,0 +1,15 @@
+public class _1015SmallestIntegerDivisibleByK {
+    class Solution {
+        public int smallestRepunitDivByK(int k) {
+            if (k % 2 == 0 || k % 5 == 0)
+                return -1;
+            int rem = 0;
+            for (int i = 1; i <= k; i++) {
+                rem = (rem * 10 + 1) % k;
+                if (rem == 0)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
