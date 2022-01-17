59153ff326ecd858c377ac221f5e3578e4e99e2b
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-01-18 02:09:55 +0530

    198 290 1137

--- LeetCode/DynamicProgramming/_1137NthTribonacciNumber.java
+++ LeetCode/DynamicProgramming/_1137NthTribonacciNumber.java
@@ -18,5 +18,23 @@ public class _1137NthTribonacciNumber {
             dp[n] = helper(n - 1, dp) + helper(n - 2, dp) + helper(n - 3, dp);
             return dp[n];
         }
+
+    }
+
+    class Solution2 {
+        public int tribonacci(int n) {
+            if (n == 0)
+                return 0;
+            if (n < 3)
+                return 1;
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+            return dp[n];
+        }
     }
 }
--- LeetCode/HashTable/_290WordPattern.java
+++ LeetCode/HashTable/_290WordPattern.java
@@ -16,4 +16,24 @@ public class _290WordPattern {
             return true;
         }
     }
+
+    class Solution2 {
+        public boolean wordPattern(String pattern, String s) {
+            String[] a = s.split(" ");
+            if (a.length != pattern.length())
+                return false;
+            HashMap<Character, String> hm = new HashMap<>();
+            HashSet<String> hs = new HashSet<>();
+            for (int i = 0; i < a.length; i++) {
+                hs.add(a[i]);
+                if (!hm.containsKey(pattern.charAt(i)))
+                    hm.put(pattern.charAt(i), a[i]);
+                else {
+                    if (!hm.get(pattern.charAt(i)).equals(a[i]))
+                        return false;
+                }
+            }
+            return hm.keySet().size() == hs.size() && true;
+        }
+    }
 }
