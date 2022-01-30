6d33b199b740492b4e48b1e510aadae2b61c4536
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-01-30 23:33:41 +0530

    2154

---
+++ LeetCode/Arrays/_2154KeepMultiplyingFoundValuesByTwo.java
@@ -0,0 +1,12 @@
+public class _2154KeepMultiplyingFoundValuesByTwo {
+    class Solution {
+        public int findFinalValue(int[] nums, int original) {
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == original)
+                    original *= 2;
+            }
+            return original;
+        }
+    }
+}
