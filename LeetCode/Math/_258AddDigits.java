8c9c75869fe927b685e8f653d5add229d8549e36
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-02-09 01:35:27 +0530

    LRU + LFU Cache

--- LeetCode/Math/_258AddDigits.java
+++ LeetCode/Math/_258AddDigits.java
@@ -17,4 +17,14 @@ public class _258AddDigits {
             return sum;
         }
     }
+
+    class Solution {
+        public int addDigits(int num) {
+            if (num == 0)
+                return 0;
+            if (num % 9 == 0)
+                return 9;
+            return num % 9;
+        }
+    }
 }
