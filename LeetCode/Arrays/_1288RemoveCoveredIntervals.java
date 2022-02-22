a57e2ef50fa0f0a09cf16f472fa1380b9b48095f
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-02-23 02:01:03 +0530

    1288

--- LeetCode/Arrays/_1288RemoveCoveredIntervals.java
+++ LeetCode/Arrays/_1288RemoveCoveredIntervals.java
@@ -1,6 +1,20 @@
 public class _1288RemoveCoveredIntervals {
     class Solution {
         public int removeCoveredIntervals(int[][] intervals) {
+            Arrays.sort(intervals, (a, b) -> (a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]));
+            int count = 0, cur = 0;
+            for (int interval[] : intervals) {
+                if (cur < interval[1]) {
+                    cur = interval[1];
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    class Solution2 {
+        public int removeCoveredIntervals(int[][] intervals) {
             List<int[]> Intervals = new ArrayList<>();
 
             for (int[] in : intervals)
