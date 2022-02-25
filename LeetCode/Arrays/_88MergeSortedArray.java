3cc225ca55b3d1c6bff3aab7f2b9fcc48c8ab290
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-02-25 19:33:25 +0530

    88 287

--- LeetCode/Arrays/_88MergeSortedArray.java
+++ LeetCode/Arrays/_88MergeSortedArray.java
@@ -5,6 +5,31 @@ import java.util.Arrays;
 public class _88MergeSortedArray {
     class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
+            if (n == 0)
+                return;
+            for (int i = 0; i < m; i++) {
+                if (nums1[i] > nums2[0]) {
+                    int temp = nums1[i];
+                    nums1[i] = nums2[0];
+                    nums2[0] = temp;
+                }
+                int f = nums2[0];
+                int k = 0;
+                for (int j = 1; j < n && nums2[j] < f; j++) {
+                    nums2[j - 1] = nums2[j];
+                    k = j;
+                }
+                nums2[k] = f;
+            }
+            int j = 0;
+            for (int i = m; i < m + n; i++) {
+                nums1[i] = nums2[j++];
+            }
+        }
+    }
+
+    class Solution2 {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
             for (int i = m, j = 0; i < nums1.length && j < nums2.length; i++, j++)
                 nums1[i] = nums2[j];
             Arrays.sort(nums1);
--- LeetCode/HashTable/_287FindDuplicateNumber.java
+++ LeetCode/HashTable/_287FindDuplicateNumber.java
@@ -1,5 +1,20 @@
 public class _287FindDuplicateNumber {
     class Solution {
+        public int findDuplicate(int[] arr) {
+            int n = arr.length;
+            int freq[] = new int[n + 1];
+            for (int i = 0; i < n; i++) {
+                if (freq[arr[i]] == 0) {
+                    freq[arr[i]] += 1;
+                } else {
+                    return arr[i];
+                }
+            }
+            return 0;
+        }
+    }
+
+    class Solution2 {
         public int findDuplicate(int[] nums) {
             HashSet<Integer> hs = new HashSet<>();
             for (int n : nums) {
@@ -10,4 +25,5 @@ public class _287FindDuplicateNumber {
             return 0;
         }
     }
+
 }
