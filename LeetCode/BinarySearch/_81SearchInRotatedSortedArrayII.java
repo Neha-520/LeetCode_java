9b31f5e57d54c4a5ffde3717a88acc043236dc49
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-03-29 00:05:16 +0530

    33 81

---
+++ LeetCode/BinarySearch/_81SearchInRotatedSortedArrayII.java
@@ -0,0 +1,26 @@
+public class _81SearchInRotatedSortedArrayII {
+    class Solution {
+        public boolean search(int[] nums, int target) {
+            int l = 0, r = nums.length - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] == target)
+                    return true;
+                if (nums[l] < nums[mid]) {
+                    if (target >= nums[l] && target < nums[mid])
+                        r = mid - 1;
+                    else
+                        l = mid + 1;
+                } else if (nums[l] > nums[mid]) {
+                    if (target > nums[mid] && target <= nums[r])
+                        l = mid + 1;
+                    else
+                        r = mid - 1;
+                } else {
+                    l += 1;
+                }
+            }
+            return false;
+        }
+    }
+}
