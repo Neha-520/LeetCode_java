22982efe7a140e741bf629e8d3eb4c3f6b4aee3d
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-03-31 01:17:41 +0530

    581

---
+++ LeetCode/TwoPointer/_581ShortestUnsortedContiuousSubarray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+}
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    r = Math.max(r, j);
+                    l = Math.min(l, i);
+                }
+            }
+        }
+        return r - l < 0 ? 0 : r - l + 1;
+    }
+}
