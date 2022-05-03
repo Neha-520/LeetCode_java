e789de7fb98830584aabed9dd99f7fd84a96f523
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-05-04 01:39:20 +0530

    581 1701

---
+++ LeetCode/Arrays/_1701AverageWaitingTime.java
@@ -0,0 +1,16 @@
+public class _1701AverageWaitingTime {
+    class Solution {
+        public double averageWaitingTime(int[][] customers) {
+            double time = 0;
+            double waitingTime = 0;
+
+            for (int[] cust : customers) {
+                time = Math.max(cust[0], time);
+                time = time + cust[1];
+                waitingTime += (time - cust[0]);
+            }
+
+            return waitingTime / customers.length;
+        }
+    }
+}
