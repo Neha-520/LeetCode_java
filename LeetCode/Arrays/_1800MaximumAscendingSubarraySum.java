16776a3bb073b119e8ab57c74acce4a45ad3a513
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2025-02-05 18:43:27 +0530

    1790

--- LeetCode/Arrays/_1800MaximumAscendingSubarraySum.java
+++ LeetCode/Arrays/_1800MaximumAscendingSubarraySum.java
@@ -14,3 +14,16 @@ public class _1800MaximumAscendingSubarraySum {
         }
     }
 }
+
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+       if(nums.length == 1) return nums[0];
+       int max = nums[0],current= nums[0];
+       for(int i = 0;i<nums.length-1;i++){
+        if(nums[i+1]>nums[i]) current+=nums[i+1];
+        else current = nums[i+1];
+        max = Math.max(max,current);
+       }
+       return max;
+    }
+}
